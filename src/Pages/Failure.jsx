c2b1ab8332f29a03ef652f1acd74dec85f77c33e
Krischal Om Pote <krischalom.pote@apexcollege.edu.np>
2023-07-24 23:35:10 +0545

    Add::Booking page done

---
+++ src/Pages/Failure.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Success = () => {
+    return (
+        <>
+            <h1>Success trasfer </h1>
+
+        </>
+    );
+};
+
+export default Success ;
+
+
+
